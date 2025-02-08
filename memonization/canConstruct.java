748372d950ed9e0da82cc268d928764468f0d280
HarshitAgrawal22 <haharshitagrawal22@gmail.com>
2025-02-08 15:58:29 +0530

    added memoized code

--- memonization/canConstruct.java
+++ memonization/canConstruct.java
@@ -1,9 +1,13 @@
+import java.util.HashMap;
+
 public class canConstruct {
     public static void main(String[] args) {
         System.out.println(canConstructRecursion("abcdef", new String[] { "ab", "abc", "cd", "def", "abcd" }));
         System.out.println(canConstructRecursion("abcdef", new String[] { "ab", "abc", "cd", "def", "abcd" }));
         System.out.println(canConstructRecursion("skateboard", new String[] { "bo", "ate", "t", "ska" }));
 
+        System.out.println(canConstructMemoized("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+                new String[] { "e", "ee", "eeef", "eeee", "eeeee", "eeeeee" }, new HashMap<String, Boolean>()));
     }
 
     static boolean canConstructRecursion(String target, String[] arr) {
@@ -14,7 +18,6 @@ public class canConstruct {
             String word = arr[i];
             if (target.indexOf(word) == 0) {
                 final String suffix = target.substring(word.length());
-                System.err.println(suffix);
                 if (canConstructRecursion(suffix, arr)) {
                     return true;
                 }
@@ -23,7 +26,24 @@ public class canConstruct {
         return false;
     }
 
-    // boolean canConstructMemoized() {
-
-    // }
+    static boolean canConstructMemoized(String target, String[] arr, HashMap<String, Boolean> map) {
+        if (map.containsKey(target)) {
+            return map.get(target);
+        }
+        if (target == "") {
+            return true;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            String word = arr[i];
+            if (target.indexOf(word) == 0) {
+                final String suffix = target.substring(word.length());
+                if (canConstructMemoized(suffix, arr, map)) {
+                    map.put(target, true);
+                    return true;
+                }
+            }
+        }
+        map.put(target, false);
+        return false;
+    }
 }
